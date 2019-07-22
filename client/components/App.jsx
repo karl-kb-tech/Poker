a7eddf485cec95f6b239ba51feb2eee4a4a1c1b4
Karl Kairakau <kairakau@gmail.com>
2019-07-22 15:41:06 +1200

    Initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+import Deck from './Deck'
+
+const App = () => {
+  return (
+    <div>
+      <Deck />
+    </div>
+  )
+}
+
+export default App
---
+++ client/components/Deck.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+var deck = [];
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function newDeck()
+{
+	var deck = new Array();
+
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < values.length; x++)
+		{
+			var card = {Value: values[x], Suit: suits[i]};
+            deck.push(card);
+            console.log(deck)
+		}
+    }
+
+
+	return deck;
+}
+
+export default newDeck
